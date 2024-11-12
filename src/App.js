2cce64ff42d66d188c01251d3b7597656318a873
shahadhf <salmarag@outlook.com>
2024-11-12 13:41:13 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import storeImage from "../src/joshua-rodriguez-f7zm5TDOi4g-unsplash.jpg";
 
 function App() {
+  const product = [
+    {
+      name: "Flat White",
+      price: " 2.100 KWD",
+    },
+    {
+      name: "Cappucino",
+      price: " 1.950 KWD",
+    },
+  ];
+
+  const productList = products.map((product, index) => {
+    return (
+      <div key={index}>
+        <img> className="Product-Image" src= {storeImage}</img>
+        <p> {product.price}</p>
+        <p> {product.name}</p>
+      </div>
+    );
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hni Roastery</h1>
+      <p> More than just Coffee </p>
+      <img className="store-Image" src={storeImage}></img>
+      <div className="Products-List">
+        {productList}
+
+        <img> className="Product-Image" src= {storeImage}</img>
+        <p> Flat White</p>
+        <p>2.000 KWD</p>
+      </div>
     </div>
   );
 }
